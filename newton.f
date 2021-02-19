71cfc9f1ae1ecf4a7b78b622280515daf02e4b89
Timofey Mukha <timofey.mukha@protonmail.com>
2021-02-19 16:41:19 +0100

    branch out newton solver to separate file

---
+++ newton.f
@@ -0,0 +1,39 @@
+!> @brief Newton-Raphson solver for non-linear algeraic equations
+!! @param[in]   u              velocity magnitude
+!! @param[in]   y              wall-normal distance
+!! @param[in]   utau           initial guess for the friction velocity
+!! @param[in]   tol            error tolerance
+!! @param[in]   maxitter       max number of iteratios
+      real function newton(f, d, u, y, utau, tol, maxiter)
+
+      implicit none
+
+      ! input
+      real f, d
+      real u, y, utau, tol
+      integer maxiter 
+
+      real deltax, fx, fxprime
+      integer k
+      
+      newton = utau
+
+      do k=1,maxiter
+
+        ! evaluate function and its derivative
+        fx = f(u, y, newton)
+        fxprime = d(u, y, newton)
+
+        if (abs(fx) < tol) then
+          exit
+        endif
+
+        ! compute Newton increment
+        deltax = fx/fxprime
+
+        ! update solution:
+        newton = newton - deltax
+
+      enddo
+
+      end
